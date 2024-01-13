abd5f4af270aa01a7b86d4b1d39f1e4923d2b009
RadouaneBaba <radoine.baba04@gmail.com>
2024-01-13 06:19:24 -0800

    last

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     """ generate archive and store it in versions dir """
     now = datetime.now()
     filename = now.strftime("versions/web_static_%Y%m%d%H%M%S.tgz")
-    command = f"tar -czvf {filename} web_static"
+    command = "tar -czvf {} web_static".format(filename)
     local("mkdir -p versions")
     try:
         local(command)
@@ -22,7 +22,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ deploy archive """
+    """ deploy archive to webservers """
     if not os.path.exists(archive_path):
         return False
 
@@ -31,21 +31,20 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/")
 
-        path_1 = f"/tmp/{name}.tgz"
-        path_2 = f"/data/web_static/releases/{name}"
+        path_1 = "/tmp/{}.tgz".format(name)
+        path_2 = "/data/web_static/releases/{}".format(name)
 
-        run(f"mkdir -p {path_2}")
-        run(f"tar -xzf {path_1} -C {path_2}")
-        run(f"rm {path_1}")
-        run(f"mv {path_2}/web_static/* {path_2}")
-        run(f"rm -rf {path_2}/web_static")
+        run("mkdir -p {}".format(path_2))
+        run("tar -xzf {} -C {}".format(path_1, path_2))
+        run("rm {}".format(path_1))
+        run("mv {}/web_static/* {}".format(path_2, path_2))
+        run("rm -rf {}/web_static".format(path_2))
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {path_2} /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path_2))
         print("New version deployed!")
         return True
     except Exception:
         return False
-
 
 def deploy():
     """ create archive and distribute it to web servers """
