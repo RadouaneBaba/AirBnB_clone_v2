25a93fe67359cc6dde41b55bd1c1c9006edd66bb
RadouaneBaba <radoine.baba04@gmail.com>
2024-01-30 10:31:46 -0800

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,7 @@ from models.state import State
 states = storage.all(State)
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """ list states """
@@ -16,10 +17,12 @@ def states_list():
         state_list.append(state)
     return render_template("7-states_list.html", states=state_list)
 
+
 @app.teardown_appcontext
 def close_teardown(error=None):
     """ close session """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
